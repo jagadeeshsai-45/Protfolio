ac5c153b28c4c4a16767ff2bad774eddb1c32e06
jagadeeshsai-45 <saijagadeesh699@gmail.com>
2025-03-03 23:29:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: 'Protfolio/',
-  plugins: [react()],
+  base: "/Protfolio/", // Repository name with a leading slash
 })
